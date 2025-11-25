96cf636cfa642db2a04edaa843558c04da19889a
jobiewong <jobietsy@gmail.com>
2025-11-25 18:25:14 +0000

    chore: disable debug mode on transcript generation

--- src/components/audio-upload/index.tsx
+++ src/components/audio-upload/index.tsx
@@ -199,7 +199,7 @@ export function AudioUpload() {
           transcription.text,
           campaign?.dmName ?? "Unknown DM",
           campaign?.players ?? [],
-          true // debug
+          false // debug
         );
 
         if (notes) {
